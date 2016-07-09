ecc784078ef5b1d0c17754acad03beaf53058b56
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-10 00:09:17 +0900

    Remove 'use'

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -2,11 +2,9 @@
 
 namespace HirotoK\JSON5\Exception;
 
-use RuntimeException as Exception;
-
 /**
  * Class RuntimeException
  *
  * @package HirotoK\JSON5\Exception
  */
-class RuntimeException extends Exception {}
+class RuntimeException extends \RuntimeException {}
