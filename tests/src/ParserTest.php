d99077d41adebf174d6ddb30a172a4e7096c1ff2
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-14 20:59:29 +0900

    Update Parser class test

--- tests/src/ParserTest.php
+++ tests/src/ParserTest.php
@@ -10,36 +10,55 @@ use stdClass;
  *
  * @package HirotoK\JSON5\Tests
  */
-class ParserTest extends Base {
+class ParserTest extends \PHPUnit_Framework_TestCase {
 
-  /**
-   * Test 'parse()' method.
-   *
-   * @return void
-   */
-  public function testParse() {
-    foreach ($this->json5_files as $path) {
-      $json5 = file_get_contents($path);
-      $parser = new Parser($json5);
-      $this->assertInstanceOf(Parser::class, $parser);
-      $json = $parser->parse();
-      $this->assertInstanceOf(stdClass::class, $json);
-    }
+  public function testParseExampleJson5()
+  {
+    $json5 = file_get_contents(JSON5_FILE_DIR."/example.json5");
+    $parser = new Parser($json5);
+    $this->assertInstanceOf(Parser::class, $parser);
+    $json = $parser->parse();
+    $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('bar', $json->foo);
+    $this->assertTrue($json->while);
+    $this->assertEquals(INF, $json->to);
   }
 
-  /**
-   * Test 'parse()' method and use assoc option.
-   *
-   * @return void
-   */
-  public function testParseAssoc() {
-    foreach ($this->json5_files as $path) {
-      $json5 = file_get_contents($path);
-      $parser = new Parser($json5);
-      $this->assertInstanceOf(Parser::class, $parser);
-      $json = $parser->parse(true);
-      $this->assertTrue(is_array($json));
-    }
+  public function testParsePackageJson5()
+  {
+    $json5 = file_get_contents(JSON5_FILE_DIR."/package.json5");
+    $parser = new Parser($json5);
+    $this->assertInstanceOf(Parser::class, $parser);
+    $json = $parser->parse();
+    $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('json5', $json->name);
+    $this->assertInstanceOf(stdClass::class, $json->contributors);
+    $this->assertTrue(isset($json->scripts->build));
+    $this->assertTrue(isset($json->scripts->test));
+  }
+
+  public function testParseWithAssocExampleJson5() {
+    $json5 = file_get_contents(JSON5_FILE_DIR."/example.json5");
+    $parser = new Parser($json5);
+    $this->assertInstanceOf(Parser::class, $parser);
+    $json = $parser->parse(true);
+    $this->assertTrue(is_array($json));
+    $this->assertEquals('bar', $json["foo"]);
+    $this->assertTrue($json["while"]);
+    $this->assertEquals(INF, $json["to"]);
+  }
+
+  public function testParseWithAssocPackageJson5()
+  {
+    $json5 = file_get_contents(JSON5_FILE_DIR."/package.json5");
+    $parser = new Parser($json5);
+    $this->assertInstanceOf(Parser::class, $parser);
+    $json = $parser->parse(true);
+    $this->assertTrue(is_array($json));
+    $this->assertEquals('json5', $json["name"]);
+    $this->assertTrue(is_array($json["contributors"]));
+    $this->assertTrue(isset($json["scripts"]["build"]));
+    $this->assertTrue(isset($json["scripts"]["test"]));
   }
 
 }
