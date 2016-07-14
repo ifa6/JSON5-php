a6c47c5636cf5990d7dbe1abf9310dc4eb807214
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-14 21:18:23 +0900

    Fix the coding style

--- src/JSON5.php
+++ src/JSON5.php
@@ -14,48 +14,48 @@ class JSON5
 {
 
     /**
-   * Decode JSON5 string.
-   *
-   * @param string $json5
-   * @param bool $assoc
-   * @return mixed
-   */
-  public static function decode($json5, $assoc = false)
-  {
-      $parser = new Parser($json5);
+     * Decode JSON5 string.
+     *
+     * @param string $json5
+     * @param bool $assoc
+     * @return mixed
+     */
+    public static function decode($json5, $assoc = false)
+    {
+        $parser = new Parser($json5);
 
-      return $parser->parse($assoc);
-  }
+        return $parser->parse($assoc);
+    }
 
-  /**
-   * Encode JSON5
-   *
-   * @param mixed $json5
-   * @return mixed
-   */
-  public static function encode($json5)
-  {
-      return json_encode($json5);
-  }
+    /**
+     * Encode JSON5
+     *
+     * @param mixed $json5
+     * @return mixed
+     */
+    public static function encode($json5)
+    {
+        return json_encode($json5);
+    }
 
-  /**
-   * Decode JSON5 by file.
-   *
-   * @param string|SplFileObject $file
-   * @param bool $assoc
-   * @return mixed
-   */
-  public static function decodeFile($file, $assoc = false)
-  {
-      if (!$file instanceof SplFileObject) {
-          if (!file_exists($file)) {
-              throw new RuntimeException("File does not exist.");
-          }
+    /**
+     * Decode JSON5 by file.
+     *
+     * @param string|SplFileObject $file
+     * @param bool $assoc
+     * @return mixed
+     */
+    public static function decodeFile($file, $assoc = false)
+    {
+        if (!$file instanceof SplFileObject) {
+            if (!file_exists($file)) {
+                throw new RuntimeException("File does not exist.");
+            }
 
-          return self::decodeFile(new SplFileObject($file, "r"), $assoc);
-      }
-      $json5 = $file->fread($file->getSize());
+            return self::decodeFile(new SplFileObject($file, "r"), $assoc);
+        }
+        $json5 = $file->fread($file->getSize());
 
-      return self::decode($json5, $assoc);
-  }
+        return self::decode($json5, $assoc);
+    }
 }
