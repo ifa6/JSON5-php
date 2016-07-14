eb5e49a49b0d5194e3a8d1f13d764b4ebde67e51
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-14 21:39:40 +0900

    Create FileNotFoundException

---
+++ src/Exception/FileNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HirotoK\JSON5\Exception;
+
+/**
+ * Class FileNotFoundException
+ *
+ * @package HirotoK\JSON5\Exception
+ */
+class FileNotFoundException extends RuntimeException
+{
+}
