d2e5a16dd39d7dd9e2148471b02fa3a86e50fb8b
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-14 20:42:53 +0900

    Add tests

--- tests/src/JSON5Test.php
+++ tests/src/JSON5Test.php
@@ -18,6 +18,8 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $json5 = file_get_contents(JSON5_FILE_DIR."/example.json5");
     $json  = JSON5::decode($json5);
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('bar', $json->foo);
+    $this->assertEquals(0.5, $json->half);
   }
 
   public function testDecodePackageJson5()
@@ -25,43 +27,60 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $json5 = file_get_contents(JSON5_FILE_DIR."/package.json5");
     $json  = JSON5::decode($json5);
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('json5', $json->name);
+    $this->assertInstanceOf(stdClass::class, $json->contributors);
+    $this->assertEquals('git', $json->repository->type);
   }
 
   public function testDecodeWithAssocExampleJson5() {
     $json5 = file_get_contents(JSON5_FILE_DIR."/example.json5");
     $json  = JSON5::decode($json5, true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('bar', $json["foo"]);
+    $this->assertEquals(0.5, $json["half"]);
   }
-
 
   public function testDecodeWithAssocPackageJson5() {
     $json5 = file_get_contents(JSON5_FILE_DIR."/package.json5");
     $json  = JSON5::decode($json5, true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('json5', $json["name"]);
+    $this->assertTrue(is_array($json["contributors"]));
+    $this->assertEquals('git', $json["repository"]["type"]);
   }
 
   public function testDecodeFileStringExampleJson5()
   {
     $json = JSON5::decodeFile(JSON5_FILE_DIR."/example.json5");
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('bar', $json->foo);
+    $this->assertEquals(0.5, $json->half);
   }
 
   public function testDecodeFileStringPackageJson5()
   {
     $json = JSON5::decodeFile(JSON5_FILE_DIR."/package.json5");
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('json5', $json->name);
+    $this->assertInstanceOf(stdClass::class, $json->contributors);
+    $this->assertEquals('git', $json->repository->type);
   }
 
   public function testDecodeFileStringWithAssocExampleJson5()
   {
     $json = JSON5::decodeFile(JSON5_FILE_DIR."/example.json5", true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('bar', $json["foo"]);
+    $this->assertEquals(0.5, $json["half"]);
   }
 
   public function testDecodeFileStringWithAssocPackageJson5()
   {
     $json = JSON5::decodeFile(JSON5_FILE_DIR."/package.json5", true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('json5', $json["name"]);
+    $this->assertTrue(is_array($json["contributors"]));
+    $this->assertEquals('git', $json["repository"]["type"]);
   }
 
   public function testDecodeFileSplExampleJson5()
@@ -69,6 +88,8 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $spl = new SplFileObject(JSON5_FILE_DIR."/example.json5");
     $json = JSON5::decodeFile($spl);
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('bar', $json->foo);
+    $this->assertEquals(0.5, $json->half);
   }
 
   public function testDecodeFileSplPackageJson5()
@@ -76,6 +97,9 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $spl = new SplFileObject(JSON5_FILE_DIR."/package.json5");
     $json = JSON5::decodeFile($spl);
     $this->assertInstanceOf(stdClass::class, $json);
+    $this->assertEquals('json5', $json->name);
+    $this->assertInstanceOf(stdClass::class, $json->contributors);
+    $this->assertEquals('git', $json->repository->type);
   }
 
   public function testDecodeFileSplWithAssocExampleJson5()
@@ -83,6 +107,8 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $spl = new SplFileObject(JSON5_FILE_DIR."/example.json5");
     $json = JSON5::decodeFile($spl, true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('bar', $json["foo"]);
+    $this->assertEquals(0.5, $json["half"]);
   }
 
   public function testDecodeFileSplWithAssocPackageJson5()
@@ -90,6 +116,9 @@ class JSON5Test extends \PHPUnit_Framework_TestCase {
     $spl = new SplFileObject(JSON5_FILE_DIR."/package.json5");
     $json = JSON5::decodeFile($spl, true);
     $this->assertTrue(is_array($json));
+    $this->assertEquals('json5', $json["name"]);
+    $this->assertTrue(is_array($json["contributors"]));
+    $this->assertEquals('git', $json["repository"]["type"]);
   }
 
 }
